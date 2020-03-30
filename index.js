49c20f73ab9d1e19d757a1094bb77e762590057b
omy86 <harrysytito@gmail.com>
2020-03-30 18:53:14 +0200

    Added /api/articles/create route

--- index.js
+++ index.js
@@ -40,3 +40,23 @@ app.get("/api/comments", (req, res) => {
         }
     );
 });
+
+app.route("/api/articles/create")
+    .get((req, res) => res.status(503).send({status: "error"}))
+    .post((req, res) => {
+        console.log(req.body);
+        const sqlConnection = mysql.createConnection(sqlConfig);
+        sqlConnection.query(
+            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)",
+            [req.body.title, req.body.content, req.body.author, req.body.created_at],
+            (error, result) => {
+                if (error) {
+                    console.log("error :", error.code);
+                    res.status(503).send({status: "error"});
+                } else {
+                    console.log(result);
+                    res.send({status: "OK"});
+                }
+                sqlConnection.end();
+            });    
+    });
