bea04316205b559f24ca654e3499acd412a37e47
omy86 <harrysytito@gmail.com>
2020-03-31 16:12:53 +0200

    correction

--- sql/00_create_tables.sql
+++ sql/00_create_tables.sql
@@ -16,3 +16,12 @@ CREATE TABLE node_comments (
     CONSTRAINT FK_comments_articles_id FOREIGN KEY (articles_id) REFERENCES node_articles(id),
     CONSTRAINT FK_comments_author FOREIGN KEY (author) REFERENCES node_users(id)
 );
+
+
+INSERT INTO node_comments(article_id, author, content) VALUES
+(1, 1, "lol"),
+(2, 1, "no mames"),
+(3, 1, "que cagado"),
+(4, 1, "quien sabe"),
+(5, 1, "esta perro"),
+(6, 1, "jijiji");
