4d5106bec3b2ab02c660d4439b2f958558dcda2a
omy86 <harrysytito@gmail.com>
2020-03-31 17:04:58 +0200

    general correction

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql   = require("mysql");
 const settings = require("./settings.json");
 
 const sqlConfig = settings.sqlConfig;
-
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server started...");
+    console.log("SERVER STARTED");
 });
 
 app.use(express.static("./public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.route("/api/articles/create")
-    .get((req, res) => res.status(503).send({status: "error"}))
+    .get((req, res) => res.status(503).send({ status: "ERROR"}))
     .post((req, res) => {
         const sqlConnection = mysql.createConnection(sqlConfig);
-        
         sqlConnection.query(
-            "INSERT INTO node_articles(title, content, author) VALUES (NULL, ?, ?, ?)",
+            "INSERT INTO node_articles(title, content, author) VALUES (?, ?, ?);",
             [ req.body.title, req.body.content, req.body.author ],
             (error, result) => {
                 if (error) {
-                    console.log("error :", error.code);
-                    res.status(503).send({status: "error"});
+                    res.status(503).send({ status: "ERROR" });
                 } else {
                     console.log(result);
-                    res.send({status: "OK"});
+                    res.send({ status: "OK" });
                 }
                 sqlConnection.end();
-            });    
+            }
+        );
     });
 
 app.route("/api/articles/delete")
-    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .get((req, res) => res.status(503).send({ status: "ERROR"}))
     .post((req, res) => {
         const sqlConnection = mysql.createConnection(sqlConfig);
-        
         sqlConnection.query(
-            "DELETE FROM node_articles WHERE id = ?",
+            "DELETE FROM node_articles WHERE id = ?;",
             [ req.body.id ],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR :", error.code);
+                    console.log(error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
                     console.log(result);
@@ -56,19 +53,19 @@ app.route("/api/articles/delete")
 
 app.get("/api/articles", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
-    
     sqlConnection.query(
-        "SELECT node_article.id, title, content, users.firstname AS authorFirstname, author.lastname AS users.lastname AS authorLastname, created_at"
+        "SELECT node_articles.id, title, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
         + "  FROM node_articles"
         + "  LEFT JOIN node_users"
         + "  ON node_articles.author = node_users.id"
         + "  ORDER BY created_at DESC"
-        + "  LIMIT 5;", 
+        + "  LIMIT 5;",
         (error, result) => {
             if (error) {
-                console.log("ERROR :", error.code);
+                res.status(503).send({ status: "ERROR" });
             } else {
-                res.send({ 
+                console.log(result);
+                res.send({
                     status: "OK",
                     articles: result,
                 });
@@ -79,36 +76,35 @@ app.get("/api/articles", (req, res) => {
 });
 
 app.route("/api/comments/create")
-    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .get((req, res) => res.status(503).send({ status: "ERROR"}))
     .post((req, res) => {
         const sqlConnection = mysql.createConnection(sqlConfig);
-        
+
         sqlConnection.query(
-            "INSERT INTO node_comments(article_id, author, content) VALUES (NULL, ?, ?, ?)",
-            [ req.body.article_id, req.body.author, req.body.content ],
+            "INSERT INTO node_comments(articles_id, author, content) VALUES (?,?,?);",
+            [req.body.articles_id, req.body.author, req.body.content],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
                     console.log(result);
                     res.send({ status: "OK" });
                 }
                 sqlConnection.end();
-            });
+            }
+        );
     });
 
 app.route("/api/comments/delete")
-    .get((req, res) => res.status(503).send({ status: "ERROR" }))
+    .get((req, res) => res.status(503).send({ status: "ERROR"}))
     .post((req, res) => {
         const sqlConnection = mysql.createConnection(sqlConfig);
-        
+
         sqlConnection.query(
             "DELETE FROM node_comments WHERE id = ?",
-            [ req.body.id ],
+            [req.body.id],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
                     console.log(result);
@@ -121,21 +117,23 @@ app.route("/api/comments/delete")
 
 app.get("/api/comments", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
-        
+
     sqlConnection.query(
-        "SELECT id, articles_id, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
+        "SELECT articles_id, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
         + "  FROM node_comments"
         + "  LEFT JOIN node_users"
         + "  ON node_comments.author = node_users.id"
-        + "  WHERE article_id = ?"
+        + "  WHERE articles_id = ?"
         + "  ORDER BY created_at DESC"
         + "  LIMIT 5;",
         [ req.query.articles_id ],
         (error, result) => {
             if (error) {
-                console.log("ERROR :", error.code);
+                console.log(error.code);
+                res.status(503).send({ status: "ERROR" });
             } else {
-                res.send({ 
+                console.log(result);
+                res.send({
                     status: "OK",
                     comments: result,
                 });
@@ -144,4 +142,3 @@ app.get("/api/comments", (req, res) => {
         }
     );
 });
-    
--- sql/00_create_tables.sql
+++ sql/00_create_tables.sql
@@ -1,27 +1,30 @@
+-- create table articles
 CREATE TABLE node_articles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
     author INT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_articles_author FOREIGN KEY (author) REFERENCES node_users(id)
 );
 
+-- create table comments
 CREATE TABLE node_comments (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    article_id INT NOT NULL,
+    articles_id INT NOT NULL,
     author INT NOT NULL,
     content TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT FK_comments_articles_id FOREIGN KEY (articles_id) REFERENCES node_articles(id),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_comments_articles_id FOREIGN KEY (articles_id) REFERENCES node_articles(id) ON DELETE CASCADE,
     CONSTRAINT FK_comments_author FOREIGN KEY (author) REFERENCES node_users(id)
 );
 
-
-INSERT INTO node_comments(article_id, author, content) VALUES
+/*INSERT INTO node_comments(article_id, author, content) VALUES
 (1, 1, "lol"),
 (2, 1, "no mames"),
 (3, 1, "que cagado"),
 (4, 1, "quien sabe"),
 (5, 1, "esta perro"),
 (6, 1, "jijiji");
+
+ALTER TABLE node_comments ADD CONSTRAINT FK_comments_articles_id FOREIGN KEY (articles)
