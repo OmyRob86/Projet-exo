09e6326d42fc64f9a689dd7c690769282dc3754d
omy86 <harrysytito@gmail.com>
2020-04-09 10:52:27 +0200

    configured /api/comment new query

--- src/api/comments.js
+++ src/api/comments.js
@@ -5,8 +5,9 @@ const app       = require("../app.js");
 app.route("/api/comments/create")
     .get((req, res) => res.status(503).send({ status: "ERROR"}))
     .post((req, res) => {
-        const sqlConnection = mysql.createConnection(sqlConfig);
+        
 
+        const sqlConnection = mysql.createConnection(sqlConfig);
         sqlConnection.query(
             "INSERT INTO node_comments(articles_id, author, content) VALUES (?,?,?);",
             [req.body.articles_id, req.body.author, req.body.content],
@@ -44,14 +45,37 @@ app.route("/api/comments/delete")
 
 app.get("/api/comments", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
+    sqlConnection.query(
+        "SELECT articles_id, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
+        + "  FROM node_comments"
+        + "  LEFT JOIN node_users"
+        + "  ON node_comments.author = node_users.id"
+        + "  ORDER BY created_at DESC"
+        + "  LIMIT 5;",
+        (error, result) => {
+            if (error) {
+                console.log(error.code);
+                res.status(503).send({ status: "ERROR" });
+            } else {
+                console.log(result);
+                res.send({
+                    status: "OK",
+                    comments: result,
+                });
+            }
+            sqlConnection.end();
+        }
+    );
+});
 
+app.get("/api/comment", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
     sqlConnection.query(
         "SELECT articles_id, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
         + "  FROM node_comments"
         + "  LEFT JOIN node_users"
         + "  ON node_comments.author = node_users.id"
         + "  WHERE articles_id = ?"
-        + "  ORDER BY created_at DESC"
         + "  LIMIT 5;",
         [ req.query.articles_id ],
         (error, result) => {
@@ -62,7 +86,7 @@ app.get("/api/comments", (req, res) => {
                 console.log(result);
                 res.send({
                     status: "OK",
-                    comments: result,
+                    comments: result[0],
                 });
             }
             sqlConnection.end();
